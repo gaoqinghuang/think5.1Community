da54864bab804b8a9c8c4dbc9fb2aee590a8a5f7
gaoqinghuang <617103154@qq.com>
2018-05-03 09:08:55 +0800

    [feature]增加注册功能

--- application/common/validate/User.php
+++ application/common/validate/User.php
@@ -7,7 +7,7 @@ use think\Validate;
 class User extends Validate
 {
     protected $rule = [
-        'name|姓名' => ['require','length'=>'5,20','chsAlphaNum'=>5],
+        'name|用户名' => ['require','length'=>'5,20','chsAlphaNum'],
         'email|邮箱' => ['require','email','unique'=>'user'],
         'password|密码' => ['require','length'=>'6,20','alphaNum','confirm'=>'confirm'],
         'mobile|手机号' => ['require','mobile','unique'=>'user'],
--- application/index/controller/Test.php
+++ application/index/controller/Test.php
@@ -5,8 +5,29 @@ namespace app\index\controller;
 
 
 use app\common\controller\Base;
+use app\common\model\User;
+
 
 class Test extends Base
 {
-
+    public function test()
+    {
+        $data = [
+            'name' => 'xiaoghao1',
+            'email'=> '6171',
+            'mobile' => '2222',
+            'password'=>'12456',
+            'is_admin'=> 0,
+        ];
+        $rs =  User::create($data);
+        dump($rs);die();
+        if($rs)
+        {
+            dump(11);
+        }
+        else
+        {
+            dump(22);
+        }
+    }
 }
